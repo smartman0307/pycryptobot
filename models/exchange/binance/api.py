a1959e1b881fbf9b08a3321814292a54fe2843c4
Haruto0307 <skybolt0307@gmail.com>
2021-09-02 22:49:24 +0100

    fixed an issue

--- models/exchange/binance/api.py
+++ models/exchange/binance/api.py
@@ -793,7 +793,7 @@ class AuthAPI(AuthAPIBase):
             if self.die_on_api_error:
                 raise SystemExit(err)
             else:
-                Logger.debug(err)
+                Logger.error(err)
                 return {}
         else:
             if self.die_on_api_error:
@@ -1053,7 +1053,7 @@ class PublicAPI(AuthAPIBase):
             if self.die_on_api_error:
                 raise SystemExit(err)
             else:
-                Logger.debug(err)
+                Logger.error(err)
                 return {}
         else:
             if self.die_on_api_error:
